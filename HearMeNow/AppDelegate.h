7894edb16a846540409cd07b0eeaac0b6b2b7139
Nenad Djukic <elref@yahoo.com>
2017-04-14 07:19:52 +0200

    Initial Commit

---
+++ HearMeNow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HearMeNow
+//
+//  Created by Nenad on 4/14/17.
+//  Copyright © 2017 Nenad.Dj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HearMeNow/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HearMeNow
+//
+//  Created by Nenad on 4/14/17.
+//  Copyright © 2017 Nenad.Dj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
